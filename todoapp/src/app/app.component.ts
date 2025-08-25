c3fa43ebeb738baaccda9858d87f7af2fa77f781
jay <india.jai@gmail.com>
2025-08-25 21:22:17 +0100

    footer added

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent, BodyComponent],
+  imports: [HeaderComponent, BodyComponent,FooterComponent],
 })
 export class AppComponent {
   title = 'todoapp';
